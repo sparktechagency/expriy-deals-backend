8653c3f09d8d1373804c25fed87ac5ba9ce69dd9
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2025-06-01 12:34:42 +0600

    Updated vendor request model

--- src/app/modules/vendorRequest/vendorRequest.models.ts
+++ src/app/modules/vendorRequest/vendorRequest.models.ts
@@ -1,35 +1,49 @@
-
 import { model, Schema } from 'mongoose';
-import { IVendorRequest, IVendorRequestModules } from './vendorRequest.interface';
+import {
+  IVendorRequest,
+  IVendorRequestModules,
+} from './vendorRequest.interface';
 
 const vendorRequestSchema = new Schema<IVendorRequest>(
   {
-    isDeleted: { type: 'boolean', default: false },
+    name: {
+      type: String,
+      require: true,
+    },
+    shopName: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    document: {
+      type: String,
+      require: true,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'approved', 'canceled'],
+      default: 'pending',
+    },
+    location: {
+      type: { type: String, default: 'Point' },
+      coordinates: [
+        {
+          type: Number,
+          require: true,
+        },
+      ],
+    },
   },
   {
     timestamps: true,
-  }
+  },
 );
-
-//vendorRequestSchema.pre('find', function (next) {
-//  //@ts-ignore
-//  this.find({ isDeleted: { $ne: true } });
-//  next();
-//});
-
-//vendorRequestSchema.pre('findOne', function (next) {
-  //@ts-ignore
-  //this.find({ isDeleted: { $ne: true } });
- // next();
-//});
-
-vendorRequestSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
-  next();
-});
 
 const VendorRequest = model<IVendorRequest, IVendorRequestModules>(
   'VendorRequest',
-  vendorRequestSchema
+  vendorRequestSchema,
 );
 export default VendorRequest;
